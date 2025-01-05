6863f324ab50fb8751c06abaf163e5c9742b94aa
Vick K <vickoch20@gmail.com>
2025-01-05 15:46:46 +0300

    update page with about import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Hero from '@/components/sections/Hero';
+import About from '@/components/sections/About';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Hero />
+      <About />
     </main>
   );
 }
